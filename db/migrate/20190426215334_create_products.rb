5b351414a06951c0ca9a67e624d01d1b15c07981
Hussein-ElAttar <hussien.elattar1@gmail.com>
2019-04-26 23:56:26 +0200

    Scaffold Product

---
+++ db/migrate/20190426215334_create_products.rb
@@ -0,0 +1,15 @@
+class CreateProducts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :products do |t|
+      t.string :title
+      t.text :description
+      t.float :price
+      t.integer :in_stock
+      t.belongs_to :category, foreign_key: true
+      t.belongs_to :brand, foreign_key: true
+      t.has_many :image
+
+      t.timestamps
+    end
+  end
+end
