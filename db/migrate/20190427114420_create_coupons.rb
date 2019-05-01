31462d4af602101cca9b10a336312958c89f4806
Abdelrahman Amin <abdelrahman.amin93@gmail.com>
2019-05-01 13:12:04 +0200

    Modify coupons and stores tables in DB

--- db/migrate/20190427114420_create_coupons.rb
+++ db/migrate/20190427114420_create_coupons.rb
@@ -3,6 +3,7 @@ class CreateCoupons < ActiveRecord::Migration[5.2]
     create_table :coupons do |t|
       t.integer :percentage
       t.datetime :expire_at
+      t.integer :usage_number
 
       t.timestamps
     end
