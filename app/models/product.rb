240ff382db8a7e9004858e523196fbf77c584f39
Abdelrahman Amin <abdelrahman.amin93@gmail.com>
2019-05-03 21:30:44 +0200

    Mount product_images uploader to product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,9 @@
 class Product < ApplicationRecord
+  mount_uploaders :product_images, ProductImagesUploader
   belongs_to :category
   belongs_to :brand
   has_many   :images
-  has_and_belongs_to_many :orders
+  # has_and_belongs_to_many :orders
   has_many :users, through: :carts
   has_many :carts
 
