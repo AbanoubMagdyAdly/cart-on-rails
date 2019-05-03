e1cb97a45bc16d89357e1ae5cacc59eea71b3d78
AbanoubMagdyAdly <abanoub.magdy.adly@gmail.com>
2019-05-03 11:02:55 +0200

    Add cancancan to activeadmin

---
+++ app/models/ability.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+    def initialize(user)
+    user ||= AdminUser.new
+    if user.role == 'admin'
+      can :manage, :all
+      can :read, ActiveAdmin::Page, name: "Dashboard", namespace_name: "admin"
+
+    else
+      can :manage, Product #, store.admin_user.id = user.id 
+      can :read, ActiveAdmin::Page, name: "Dashboard", namespace_name: "admin"
+
+    end
+  end
+end
