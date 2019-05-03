190f5409d49396f3123bc2762eae82b439860c21
nour <nour.kilany88@gmail.com>
2019-05-03 18:29:57 +0200

    Seed user into the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(email: 'user@gmail.com', name: 'User', password: "password", address: "1 street address", avatar: "avatar/path")
 AdminUser.create!(  email: 'admin@example.com',
                     password: 'password',
                     password_confirmation: 'password' ,
