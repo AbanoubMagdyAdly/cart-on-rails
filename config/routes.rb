8fd3f395ed84f5dcd3999ac5aa2a56f885682166
AbanoubMagdyAdly <abanoub.magdy.adly@gmail.com>
2019-05-03 23:39:48 +0200

    Add carts and order routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   resources :products
   resources :brands
   resources :categories
+  resources :carts
+  resources :orders
   get '/*a', to: 'application#not_found'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
