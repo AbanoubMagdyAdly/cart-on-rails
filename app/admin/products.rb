27afa90c7825d71331c59aec2d2823c4e42166d7
Abdelrahman Amin <abdelrahman.amin93@gmail.com>
2019-05-03 21:34:19 +0200

    Add multiple image to product

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -2,7 +2,20 @@ ActiveAdmin.register Product do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-permit_params :title, :category_id, :brand_id, :description, :price, :in_stock
+permit_params :title, :category_id, :brand_id, :description,:price, :in_stock, product_images:[]
+form html: { multipart: true } do |f|
+    f.inputs do
+      f.input :category
+      f.input :brand
+      f.input :title
+      f.input :description
+      f.input :price
+      f.input :in_stock
+      f.input :product_images, as: :file, input_html: { multiple: true }
+    end
+    actions
+  end
+
 # permit_params :list, :of, :attributes, :on, :model
 #
 # or
