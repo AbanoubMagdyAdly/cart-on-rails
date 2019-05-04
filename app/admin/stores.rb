dd168ed980f14e722002660f034a7fc43bfd66de
Abdelrahman Amin <abdelrahman.amin93@gmail.com>
2019-05-04 23:51:58 +0200

    Add store summary as permit_prams

--- app/admin/stores.rb
+++ app/admin/stores.rb
@@ -2,12 +2,13 @@ ActiveAdmin.register Store do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-permit_params :name, :admin_user_id
+permit_params :name, :admin_user_id ,:summary
 # permit_params :list, :of, :attributes, :on, :model
 #
 # or
 #
-# permit_params do
+# permit_params do  has_and_belongs_to_many :orders
+
 #   permitted = [:permitted, :attributes]
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
