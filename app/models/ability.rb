6ddb4451b211328eb40d84889c2deabd304138d9
Abdelrahman Amin <abdelrahman.amin93@gmail.com>
2019-05-04 23:50:40 +0200

    Fix admin and seller permissions

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,17 +7,17 @@ class Ability
     user ||= AdminUser.new
     if user.role == 'admin'
       # can :manage, :all
-      can :manage, Store 
-      can :manage, Category
-      can :manage, Brand
-      can :manage, Coupon
-      can :manage, AdminUser
+      can [:read, :update ,:create], Store 
+      can [:read, :update ,:create], Category
+      can [:read, :update ,:create], Brand
+      can [:read, :update ,:create], Coupon
+      can [:read, :update ,:create], AdminUser
       can :read, ActiveAdmin::Page, name: "Dashboard", namespace_name: "admin"
 
     else
       can [:read, :update ,:destroy], Product , :store_id => user.store.id
       can [:create] , Product 
-      can [:read, :update], Order
+      can [:read, :update], Order , :store_id => user.store.id
       # can [:read, :update], Store , :id => user.store.id
       can :read, ActiveAdmin::Page, name: "Dashboard", namespace_name: "admin"
 
