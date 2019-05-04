467dc36ef51f2e760931177f12c8d9ddd78047d9
AbanoubMagdyAdly <abanoub.magdy.adly@gmail.com>
2019-05-04 22:25:33 +0200

    Fix admin dashboard

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,12 +6,19 @@ class Ability
     def initialize(user)
     user ||= AdminUser.new
     if user.role == 'admin'
-      can :manage, :all
+      # can :manage, :all
+      can :manage, Store 
+      can :manage, Category
+      can :manage, Brand
+      can :manage, Coupon
+      can :manage, AdminUser
       can :read, ActiveAdmin::Page, name: "Dashboard", namespace_name: "admin"
 
     else
-      can :manage, Product , :store_id => user.store.id
+      can [:read, :update ,:destroy], Product , :store_id => user.store.id
+      can [:create] , Product 
       can [:read, :update], Order
+      # can [:read, :update], Store , :id => user.store.id
       can :read, ActiveAdmin::Page, name: "Dashboard", namespace_name: "admin"
 
     end
