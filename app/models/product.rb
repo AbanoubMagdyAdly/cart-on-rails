826e0059b1555bc81c6f3daab16ad8e5bd38ef0c
Abdelrahman Amin <abdelrahman.amin93@gmail.com>
2019-05-04 16:56:06 +0200

    Add product fields validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,6 +7,12 @@ class Product < ApplicationRecord
   # has_and_belongs_to_many :orders
   has_many :users, through: :carts
   has_many :carts
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :price, presence: true  ,:numericality => { greater_than_or_equal_to: 0 }
+  validates :in_stock, presence: true  ,:numericality => { greater_than_or_equal_to: 0 }
+  
+
 
   
 end
