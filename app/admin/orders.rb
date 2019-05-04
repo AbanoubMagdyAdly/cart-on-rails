5d41ac5dd67f6b000a45dc1835090721ee170f3d
Abdelrahman Amin <abdelrahman.amin93@gmail.com>
2019-05-04 18:53:34 +0200

    Fix order update permission to seller

--- app/admin/orders.rb
+++ app/admin/orders.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register Order do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-# permit_params :user
+permit_params :user_id, :state_id, :total_paid, :coupon_id 
 #
 # or
 #
