42796fdbecc7dd249355df5d6acca1418e02880d
Abdelrahman Amin <abdelrahman.amin93@gmail.com>
2019-05-06 16:16:07 +0200

    Recreate coupon table

--- db/migrate/20190427114420_create_coupons.rb
+++ db/migrate/20190427114420_create_coupons.rb
@@ -1,10 +1,11 @@
 class CreateCoupons < ActiveRecord::Migration[5.2]
   def change
     create_table :coupons do |t|
-      t.integer :percentage
+      t.integer :amount ,default: 0
       t.datetime :expire_at
-      t.integer :usage_number
-
+      t.integer :limit ,default: 0
+      t.integer :used ,default: 0, null: false
+      t.boolean :percentage ,default: false
       t.timestamps
     end
   end
