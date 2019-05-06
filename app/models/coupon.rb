a550d834536a9665e18ed55ed35e839889b5476e
Abdelrahman Amin <abdelrahman.amin93@gmail.com>
2019-05-06 17:55:43 +0200

    Modify coupon functions

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -6,21 +6,23 @@ class Coupon < ApplicationRecord
   
   def is_valid?(code)
       coupon=Coupon.find_by(code: code)
-        (coupon.expiration.nil? || coupon.expiration >= Date.current) &&
+        (coupon.expire_at.nil? || coupon.expire_at >= Date.current) &&
         (coupon.limit == 0 || coupon.limit > coupon.used)
   end
-  def discounted_price(price)
-      new_price = if is_valid?
-                    if percentage
-                      price.to_f - (price.to_f * (amount.to_f/100))
+  def discounted_price(price,code)
+      coupon=Coupon.find_by(code: code)
+      new_price = if is_valid?(code)
+                    if coupon.percentage
+                      price.to_f - (price.to_f * (coupon.amount.to_f/100))
                     else
-                      (price.to_f - amount.to_f)
+                      (price.to_f - coupon.amount.to_f)
                     end
                   else
                     price
                   end
-  return price.floor
+  return new_price.floor
   end
+
 
 
   def display_name
