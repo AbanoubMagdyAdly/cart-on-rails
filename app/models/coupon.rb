3c87974de14ab9480e2b64584953281ed1f15fe8
Abdelrahman Amin <abdelrahman.amin93@gmail.com>
2019-05-06 16:17:28 +0200

    Add coupon validations

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,5 +1,10 @@
 class Coupon < ApplicationRecord
-    def display_name
+  validates :amount, presence: true ,:numericality => { greater_than_or_equal_to: 1 ,only_integer: true }
+  validates :code, presence: true ,uniqueness: true
+  validates :limit, presence: true
+  validates :expire_at , presence: true
+  
+  def display_name
      code + " : " + percentage.to_s + "%"
-   end
+  end
 end
