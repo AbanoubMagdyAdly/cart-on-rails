4735f5c212112dc692abd7166b1d7090890aa8dc
Abdelrahman Amin <abdelrahman.amin93@gmail.com>
2019-05-06 17:27:39 +0200

    Add custmized coupon form to active admin

--- app/admin/coupons.rb
+++ app/admin/coupons.rb
@@ -2,7 +2,17 @@ ActiveAdmin.register Coupon do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-permit_params :code, :percentage, :expire_at , :usage_number
+permit_params :code, :percentage, :expire_at  ,:amount ,:limit
+form do |f|
+    f.inputs do
+      f.input :code
+      f.input :expire_at
+      f.input :limit
+      f.input :amount
+      f.input :percentage
+    end
+    actions
+  end
 #
 # or
 #
