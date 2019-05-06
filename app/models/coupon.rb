74ed47fc7a16d452768a53e5d680ce31c7ee0502
Abdelrahman Amin <abdelrahman.amin93@gmail.com>
2019-05-06 16:56:43 +0200

    Add coupon helper functions

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -4,7 +4,26 @@ class Coupon < ApplicationRecord
   validates :limit, presence: true
   validates :expire_at , presence: true
   
+  def is_valid?(code)
+      coupon=Coupon.find_by(code: code)
+        (coupon.expiration.nil? || coupon.expiration >= Date.current) &&
+        (coupon.limit == 0 || coupon.limit > coupon.used)
+  end
+  def discounted_price(price)
+      new_price = if is_valid?
+                    if percentage
+                      price.to_f - (price.to_f * (amount.to_f/100))
+                    else
+                      (price.to_f - amount.to_f)
+                    end
+                  else
+                    price
+                  end
+  return price.floor
+  end
+
+
   def display_name
-     code + " : " + percentage.to_s + "%"
+    code   
   end
 end
